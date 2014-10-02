2edaf2a562721b9f4b5b9c82cda46fe97e4c148a
Dmitry Vislov <vdv73rus@gmail.com>
2014-10-02 15:40:09 +0400

    Base top freeze header and left freeze column added.

--- app/coffee/application.js.coffee
+++ app/coffee/application.js.coffee
@@ -1,2 +1,34 @@
 $(document).ready ->
-  $("#excel_table").jScrollPane()
+
+  srcTableWrap = $("#excel_table")
+
+  scrollPane = srcTableWrap
+    .bind 'jsp-scroll-x', (event, scrollPositionX, isAtLeft, isAtRight) ->
+      $('.freeze-table-left-wrap').css
+        left: scrollPositionX
+    .bind 'jsp-scroll-y', (event, scrollPositionY, isAtTop, isAtBottom) ->
+      $('.freeze-table-top-wrap').css
+        top: scrollPositionY
+    .jScrollPane()
+
+
+
+  initTableFirstHeader = ->
+    $('table.table:first').clone().addClass('freeze-table-top').appendTo('.table-wrap')
+
+    $('.freeze-table-top').wrap('<div class="freeze-table-top-wrap"></div>')
+    $('.freeze-table-top-wrap').css
+      height: $('table.table:first thead').height()
+
+  initTableFirstColumn = ->
+    $('table.table:first').clone().addClass('freeze-table-left').appendTo('.table-wrap')
+    $('.freeze-table-left').wrap('<div class="freeze-table-left-wrap"></div>')
+
+    columnBorderWidth = parseInt($('table.table:first tbody tr:first td:first').css("borderWidth"))
+    columnWidth = parseInt($('table.table:first thead th:first').outerWidth())
+
+    $('.freeze-table-left-wrap').css
+      width: columnWidth + columnBorderWidth + "px"
+
+  initTableFirstColumn()
+  initTableFirstHeader()
--- app/compile/js/application.js
+++ app/compile/js/application.js
@@ -1,6 +1,35 @@
 (function() {
   $(document).ready(function() {
-    return $("#excel_table").jScrollPane();
+    var initTableFirstColumn, initTableFirstHeader, scrollPane, srcTableWrap;
+    srcTableWrap = $("#excel_table");
+    scrollPane = srcTableWrap.bind('jsp-scroll-x', function(event, scrollPositionX, isAtLeft, isAtRight) {
+      return $('.freeze-table-left-wrap').css({
+        left: scrollPositionX
+      });
+    }).bind('jsp-scroll-y', function(event, scrollPositionY, isAtTop, isAtBottom) {
+      return $('.freeze-table-top-wrap').css({
+        top: scrollPositionY
+      });
+    }).jScrollPane();
+    initTableFirstHeader = function() {
+      $('table.table:first').clone().addClass('freeze-table-top').appendTo('.table-wrap');
+      $('.freeze-table-top').wrap('<div class="freeze-table-top-wrap"></div>');
+      return $('.freeze-table-top-wrap').css({
+        height: $('table.table:first thead').height()
+      });
+    };
+    initTableFirstColumn = function() {
+      var columnBorderWidth, columnWidth;
+      $('table.table:first').clone().addClass('freeze-table-left').appendTo('.table-wrap');
+      $('.freeze-table-left').wrap('<div class="freeze-table-left-wrap"></div>');
+      columnBorderWidth = parseInt($('table.table:first tbody tr:first td:first').css("borderWidth"));
+      columnWidth = parseInt($('table.table:first thead th:first').outerWidth());
+      return $('.freeze-table-left-wrap').css({
+        width: columnWidth + columnBorderWidth + "px"
+      });
+    };
+    initTableFirstColumn();
+    return initTableFirstHeader();
   });
 
 }).call(this);
